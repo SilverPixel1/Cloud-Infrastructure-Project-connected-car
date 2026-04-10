a4634e2fde52cd4e0923a701f94c10f18aa2c6a9
“SilverPixel1“ <info@projectsilverpixel.com>
2026-04-10 21:43:47 +0200

    NAT Gateway Private RT

--- connected_car_vpc.tf
+++ connected_car_vpc.tf
@@ -1,6 +1,6 @@
-###############
+#######################################################################################################################
 #Terraform configuration for connected car project
-###############
+#######################################################################################################################
 
 # Configure the AWS provider
 
@@ -45,6 +45,11 @@ variable "aws_region" {
  type = list(string)
  default = ["eu-central-1a", "eu-central-1b"]
  }
+
+#######################################################################################################################
+#Build the Networking infrastructure for the connected car project: VPC, subnets, route tables, internet gateway, NAT gateway
+#######################################################################################################################
+
 
 ##################
 #VPC
@@ -134,15 +139,68 @@ resource "aws_route_table" "connected_car_public_rt" {
   }
 }
 
-#################
-#Associate public subnets with route table (connect Subnet with Route Table)
-#################
+
+# connect public subnets with public route table
+
 resource "aws_route_table_association" "connected_car_public_rt_assoc" {
     count = length(var.availability_zones)    
     subnet_id = aws_subnet.connected_car_public_subnet[count.index].id
     route_table_id = aws_route_table.connected_car_public_rt.id
 }
 
+################### 
+# NAT Gateway for private subnets
+###################
+resource "aws_eip" "connected_car_nat_eip" {
+  domain = "vpc"
+
+
+  tags = {
+    Name = "${var.project_name}-nat-eip"
+    Project = var.project_name
+    Environment = "Development"
+  }
+} 
+
+resource "aws_nat_gateway" "connected_car_nat_gw" { 
+  allocation_id = aws_eip.connected_car_nat_eip.id
+  subnet_id = aws_subnet.connected_car_public_subnet[0].id #NAT Gateway muss in einem öffentlichen Subnetz platziert werden
+
+  tags = {
+    Name = "${var.project_name}-nat-gw"
+    Project = var.project_name
+    Environment = "Development"
+  }
+
+  depends_on = [ aws_internet_gateway.connected_car_igw] #NAT Gateway benötigt Internet Gateway, um zu funktionieren
+
+}
+
+#################
+# Route table for private subnets
+#################
+resource "aws_route_table" "connected_car_private_rt" {
+  vpc_id = aws_vpc.connected_car_vpc.id 
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.connected_car_nat_gw.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-private-rt"
+    Project = var.project_name
+    Environment = "Development"
+  }
+}
+
+
+# connect private subnets with private route tables
+resource "aws_route_table_association" "connected_car_private_rt_assoc" { 
+    count           =       length(var.availability_zones)    
+    subnet_id       =       aws_subnet.connected_car_private_subnet[count.index].id
+    route_table_id  =       aws_route_table.connected_car_private_rt.id
+}
 
 ###################
 #Output VPC and subnet IDs
@@ -158,4 +216,17 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = aws_subnet.connected_car_private_subnet[*].id
 }   
+
+#######################################################################################################################
+#Build the Infrastructure for the Project: ECR, ECS, Fargate(EC2), DynamoDB, ELB, S3, CloudWatch, IAM
+#######################################################################################################################
+
+# ECR speichert Docker Images, wie Docker HUB, privat in AWS
+# ECS ist der Container Orchestrator von AWS er startet, skaliert und verwaltet Container
+# Fargate ist der Compute Service von AWS, für serverless und günstiger (EC2 wäre für vollständige Kontrolle)
+# DynamoDB ist die NoSQL Datenbank von AWS (schnell, skalierbar, serverless)
+# ELB ist der Elastic Load Balancer von AWS (verteilt den Traffic auf mehrere Instanzen, um hohe Verfügbarkeit zu gewährleisten)
+# S3 ist der Object Storage Service von AWS (speichert Dateien, Bilder, Videos, etc.)
+# CloudWatch ist der Monitoring Service von AWS (überwacht die Infrastruktur, sammelt Logs, erstellt Alarme, etc.)
+# IAM ist der Identity and Access Management Service von AWS (verwalten von Benutzern, Rollen, Berechtigungen, etc.)  
 
